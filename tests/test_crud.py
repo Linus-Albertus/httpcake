f744c647dfe25e4997ce9c5b1b5afd962a0e7230
Lino A. Urdaneta F <lino.urdaneta@gmail.com>
2024-08-18 10:19:06 -0400

    Add some basic testing

---
+++ tests/test_crud.py
@@ -0,0 +1,45 @@
+"""CRUD operations related tests."""
+import json
+from click.testing import CliRunner
+
+from crud import create, ls
+from httpcake import cli
+
+
+runner = CliRunner()
+
+with open("./tests/fixtures/requests.json", "r") as file:
+    existing_data = json.load(file)
+
+
+def test_create():
+    result = runner.invoke(cli, ["create", "--name", "Test Request", "--request",  "http httpie.io/hello"])
+    assert result.exit_code == 0
+    assert result.output == 'Request saved.\n'
+
+
+def test_ls():
+    result = runner.invoke(cli, ["ls"])
+    assert result.exit_code == 0
+
+
+def test_run():
+    pass
+
+
+def test_delete_ok():
+    result = runner.invoke(cli, ["delete", "--request", 0])
+    assert result.exit_code == 0
+
+
+def test_delete_negatives():
+    result = runner.invoke(cli, ["delete", "--request", -1])
+    assert result.exit_code == 0
+    assert result.output == "You must enter a positive integer or zero.\n"
+
+
+def test_delete_out_of_range():
+    result = runner.invoke(cli, ["delete", "--request", 1000])
+    assert result.exit_code == 0
+    assert result.output == "Request out of range!\n"
+
