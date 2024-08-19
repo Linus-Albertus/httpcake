e6b3c365e0b713ef399177828c09e7ffc4a98807
Lino A. Urdaneta F <lino.urdaneta@gmail.com>
2024-08-19 08:59:50 -0400

    Update tests

--- tests/test_crud.py
+++ tests/test_crud.py
@@ -1,21 +1,21 @@
 """CRUD operations related tests."""
+
 import json
+
 from click.testing import CliRunner
 
 from crud import create, ls
 from httpcake import cli
 
-
 runner = CliRunner()
-
-with open("./tests/fixtures/requests.json", "r") as file:
-    existing_data = json.load(file)
 
 
 def test_create():
-    result = runner.invoke(cli, ["create", "--name", "Test Request", "--request",  "httpie.io/hello"])
+    result = runner.invoke(
+        cli, ["create", "--name", "Test Request", "--request", "httpie.io/hello"]
+    )
     assert result.exit_code == 0
-    assert result.output == 'Request saved.\n'
+    assert result.output == "Request saved.\n"
 
 
 def test_ls():
@@ -42,4 +42,3 @@ def test_delete_out_of_range():
     result = runner.invoke(cli, ["delete", "--request", 1000])
     assert result.exit_code == 0
     assert result.output == "Request out of range!\n"
-
